6d978916fe7b25648558663baf841881e58fbef1
Rii <129301857+RimjhimD@users.noreply.github.com>
2023-12-08 21:52:06 +0600

    Hello World(2)

---
+++ TestDay1/Hello World.java
@@ -0,0 +1,9 @@
+class FirstClass{
+
+    public static void main(String args[]){
+
+        System.out.println("Hello World with Java");
+        System.out.print("Hello World\n");
+        System.out.print("Test Test");
+  }
+}
