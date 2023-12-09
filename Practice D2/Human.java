4814121a68948df0661f3cd4b2213a3162ddd164
Rii <129301857+RimjhimD@users.noreply.github.com>
2023-12-09 22:46:04 +0600

    learning abt Class nd Constructor

---
+++ Practice D2/Human.java
@@ -0,0 +1,15 @@
+public class Human {
+
+    String name;
+    int age;
+    double weight;
+
+    Human(String name,int age,double weight){  //created constructor by class name Human 
+
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+
+    }
+    
+}
