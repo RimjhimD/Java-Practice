b625220da16268a2a134948dfa6860ee7cff3af4
Rii <129301857+RimjhimD@users.noreply.github.com>
2023-12-09 22:45:13 +0600

    learning abt Class nd Constructor

---
+++ Practice D2/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Human human1 = new Human("Rii",21,60);  // passing arguements
+        Human human2 = new Human("Sab",20,50);
+
+        System.out.println(human1.name);
+        System.out.println(human2.name);
+    }
+    
+}
